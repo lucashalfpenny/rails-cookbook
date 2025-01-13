5efad337d383a1daf66395177609a2cf0670a126
lucas halfpenny <lucashalfpenny@gmail.com>
2025-01-13 11:56:44 +0000

    Added seed file with recipes

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :recipe_id, uniqueness: {scope: :category_id, message: "Recipe already in list"}
+  validates :comment, length: { minimum: 6, message: "Must have at least 6 characters" }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, numericality: { in: 0..10 }
 end
